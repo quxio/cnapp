255c1291c49bbfd21f3adcdb8feb167cc2172265
quxio <jaydenjihochoi@gmail.com>
2026-08-08 14:07:07 -0700

    initial

---
+++ README.md
@@ -0,0 +1,15 @@
+# cnapp
+
+my congressional app challenge web app
+
+---
+
+current working title - "Idea4Project"
+A site where user-generated project ideas/tutorials get upvosts/downvotes
+
+---
+
+*TODO*
+
+- Set up nav
+- 
---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
+import { ConvexReactClient } from "convex/react";
+import "./index.css";
+
+const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL);
+
+createRoot(document.getElementById("REACTROOT")).render(
+  <StrictMode>
+    <ConvexAuthProvider client={convex}>
+      <div>hello there!</div>
+    </ConvexAuthProvider>
+  </StrictMode>,
+);

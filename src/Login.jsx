d16488c9ad2ea96057ad6d4fb2ff80242dadc441
quxio <jaydenjihochoi@gmail.com>
2026-08-15 15:54:01 -0700

    LOGIN REALLY ACTUALLY FINALLY TOTALLY WORKRKKSKSKSKSKKS!

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,30 +2,38 @@ import { useState } from "react";
 import { useAuthActions } from "@convex-dev/auth/react";
 import { NavLink, useNavigate } from "react-router";
 import { Authenticated, Unauthenticated } from "convex/react";
-
+import { ConvexError } from "convex/values"
 
 export default function Login() {
 
   const navi = useNavigate();
   const auth = useAuthActions();
   let [up, setUp] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
 
   return (
     <>
       <div className="LOGINOUTERCONTAINER">
         <div className="LOGININNERCONTAINER">
-          <button onClick={()=>setUp(!up)}>{up ? "Signing UP rn" : "Signing IN rn"}</button>
-          <form onSubmit={(e)=>{
-            e.preventDefault();
-            const fd = new FormData(e.currentTarget);
-            fd.set("flow", up ? "signUp" : "signIn");
-            try {
-              auth.signIn("password", fd).catch((e)=>console.log(error.message, "GOT ERRPORIORE"));
-            } catch (e) {
-              return;
-            }
-            console.log("moving on");
-          }}>
+          <button onClick={() => setUp(!up)}>
+            {up ? "Signing UP rn" : "Signing IN rn"}
+          </button>
+          <form
+            onSubmit={async (e) => {
+              e.preventDefault();
+              const fd = new FormData(e.currentTarget);
+              fd.set("flow", up ? "signUp" : "signIn");
+              try {
+                await auth.signIn("password", fd);
+                navi("/");
+              } catch (e) {
+                if (e instanceof ConvexError) {
+                  setErrorMsg(`convexerror:\n${e.message}`);
+                }
+              }
+            }}
+          >
+            {errorMsg && <div style={{color: "coral"}}>{errorMsg}</div>}
 
             <label htmlFor="email">email</label>
             <input name="email" type="email" />
@@ -34,13 +42,17 @@ export default function Login() {
             <input name="password" type="password" />
 
             <>
-              {up ? <>
-                <label htmlFor="name">name</label>
-                <input name="name" type="text" />
+              {up ? (
+                <>
+                  <label htmlFor="name">name</label>
+                  <input name="name" type="text" />
 
-                <label htmlFor="handle">handle</label>
-                <input name="handle" type="text" />
-              </> : <></>}
+                  <label htmlFor="handle">handle</label>
+                  <input name="handle" type="text" />
+                </>
+              ) : (
+                <></>
+              )}
             </>
 
             {/*<input name="flow" value={up ? "signUp" : "signIn"} type="hidden" />*/}
@@ -48,20 +60,20 @@ export default function Login() {
             <button type="submit">submit</button>
 
             <Authenticated>
-              <div style={{backgroundColor: "cornflowerblue"}}>AUTHENTICATED</div>
-
-
-
+              <div style={{ backgroundColor: "cornflowerblue" }}>
+                AUTHENTICATED
+              </div>
             </Authenticated>
             <Unauthenticated>
-              <div style={{backgroundColor: "lightgreen"}}>UNAUTHENTICATED</div>
+              <div style={{ backgroundColor: "lightgreen" }}>
+                UNAUTHENTICATED
+              </div>
             </Unauthenticated>
             <button onClick={auth.signOut}>log out</button>
-
           </form>
           <NavLink to="/">Back</NavLink>
         </div>
       </div>
     </>
-  )
+  );
 }
